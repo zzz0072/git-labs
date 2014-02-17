c9485bc6889a69fa134d8287122c55e7c19103d0
Wen.Liao <zzz00072@gmail.com>
2014-02-17 22:55:42 +0800

    Implement 5 buggy functions in lib/lib_sample.c

--- lib/lib_sample.c
+++ lib/lib_sample.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void bug1(void)
 {
-    printf("%s\n", __FUNCTION__);
+    int *p = 1000;
+
+    *p = 10;
+    printf("%s: %d\n", __FUNCTION__, *p);
 }
 
 void bug2(void)
 {
-    printf("%s\n", __FUNCTION__);
+    char *p;
+
+    strcpy(p, "Hello world\n");
+
+    printf("%s:%s\n", __FUNCTION__, p);
+
 }
 
 void bug3(void)
 {
-    printf("%s\n", __FUNCTION__);
+    char *p="Hello world\n";
+
+    free(p);
+    printf("%s:%s\n", __FUNCTION__, p);
 }
 
 void bug4(void)
 {
-    printf("%s\n", __FUNCTION__);
+    char *p="Hello world\n";
+    char dst[4];
+
+    strcpy(dst, p);
+    printf("%s:%s\n", __FUNCTION__, dst);
 }
 
 void bug5(void)
 {
-    printf("%s\n", __FUNCTION__);
+    char str = {'H','e','l','l','o',' ','W','o','r','l','d','\n' };
+    char str2[64];
+    strcpy(str2, str);
+    printf("%s:%s\n", __FUNCTION__,str2);
 }
 
 
